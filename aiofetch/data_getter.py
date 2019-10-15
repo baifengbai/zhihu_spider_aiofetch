61214a46d73adac5ed5b140147ef1edadc2c47af
Strangestring <1814232115@qq.com>
2019-10-15 22:01:56 +0800

    Update data_getter.py

--- aiofetch/data_getter.py
+++ aiofetch/data_getter.py
@@ -3,19 +3,20 @@
 # author : strangestring
 # github : https://github.com/strangestring
 
-import zhihu_APIs
-import workflow_load_balance
-import headers_pool
-from multiprocessing import Pool, Manager
-import logging
 import asyncio
-import aiohttp
+import logging
+# import multiprocessing
 import random
 import time
-import sys
+import traceback
+from multiprocessing import Manager, Pool
+import aiohttp
+import headers_pool
+import workflow_load_balance
+import zhihu_APIs
 
 
-sys.path.append('./aiofetch')
+# sys.path.append('./aiofetch')
 
 # default args
 PROCESS_NUM = 4
@@ -37,37 +38,43 @@ async def _fetch(url: str, identifier: str or int, func: callable,
     :param func: function that return url
     :param session: aiohttp.ClientSession
     :param headers: a random selection of headers
-    :param args: may contain 'offset' or 'limit', for raise_error positioning
+    :param kwargs: might contain 'offset' or 'limit', for raise_error positioning
     :return:
     """
-    async with session.get(url, headers=headers, ssl=False) as resp:
-        # verify_ssl is deprecated, use ssl=False instead
-        status = resp.status
-        if func.__name__ == 'log':  # 已添加question/log支持(type:html)
-            if status == 200:
-                return {'identifier': identifier, 'html': await resp.text()}
+    try:
+        async with session.get(url, headers=headers, ssl=False) as resp:
+            # verify_ssl is deprecated, use ssl=False instead
+            status = resp.status
+            if isinstance(func, zhihu_APIs.TextAPI):
+                if status == 200:
+                    return {'identifier': identifier, 'html': await resp.text()}
+                else:
+                    logging.warning(
+                        f'{func.__init__.__name__}\t{identifier}\n{url} kwargs={kwargs}\nSTATUS CODE:{status}')
+                    return {'identifier': identifier, 'html': None,
+                            'status_code': status}
             else:
-                logging.warning(
-                    f'{func.__name__}\t{identifier} kwargs={kwargs}\tSTATUS CODE:{status}')
-                return {'identifier': identifier, 'html': None,
-                        'status_code': status}
-        else:
-            if status == 200:
-                _json = await resp.json(encoding='utf-8')
-                if 'paging' in _json:  # list类API
-                    _json['paging']['identifier'] = identifier
-                else:  # info类API
-                    _json['identifier'] = identifier
-            else:
-                # todo: record occasional errors and automatic retries
-                # mainly caused by human verification or '500 Internal Server Error'/'账号已停用'/'账号已注销'/'内容已被删除')
-                # just care about status code, _json is personalized
-                logging.warning(
-                    f'{func.__name__}\t{identifier} kwargs={kwargs}\tSTATUS CODE:{status}')
-                _json = {'paging': {'identifier': identifier, 'status_code': status}, 'data': {
-                    f'{func.__name__}\t{identifier} kwargs={kwargs}\tSTATUS CODE:{status}'}}
-                ...
-            return _json
+                if status == 200:
+                    _json = await resp.json(encoding='utf-8')
+                    if 'paging' in _json:  # list类API
+                        _json['paging']['identifier'] = identifier
+                    else:  # info类API
+                        _json['identifier'] = identifier
+                else:
+                    # todo: record occasional errors and automatic retries
+                    # mainly caused by human verification or '500'/'账号已停用'/'账号已注销'/'内容已被删除')
+                    # just care about status code, _json is personalized
+                    logging.warning(
+                        f'{func.__init__.__name__}\t{identifier}\n{url} kwargs={kwargs}\nSTATUS CODE:{status}')
+                    _json = {'paging': {'identifier': identifier,
+                                        'status_code': status,
+                                        'ret_json': await resp.json(
+                                            encoding='utf-8')}, 'data': {
+                        f'{func.__init__.__name__}\t{identifier} kwargs={kwargs}\tSTATUS CODE:{status}'}}
+                    ...
+                return _json
+    except BaseException:
+        logging.error(traceback.format_exc())
 
 
 async def _gather_result(work_flow: dict, func: callable, headers_pool: list,
@@ -78,13 +85,13 @@ async def _gather_result(work_flow: dict, func: callable, headers_pool: list,
     :param headers_pool: headers pool for random selection
     :return: list that contain jsons from _fetch()
     """
-    if 'limit' in func.__code__.co_varnames:
-        func_limit = func.__defaults__[func.__code__.co_varnames.index(
-            'limit') - 2]  # self, url_token/answer_id is required
-    else:
-        func_limit = 1
-        ...
-    if work_flow['task_count'] > 0:
+    try:
+        if 'limit' in func.__init__.__code__.co_varnames:
+            func_limit = func.__init__.__defaults__[
+                func.__init__.__code__.co_varnames.index(
+                    'limit') - 2]  # self, url_token/answer_id is required
+        else:
+            func_limit = 1
         async with aiohttp.ClientSession(
                 connector=aiohttp.TCPConnector(ssl=False)) as session:
             task_pool = []
@@ -94,7 +101,7 @@ async def _gather_result(work_flow: dict, func: callable, headers_pool: list,
                     query_args = task['query_args']
                 else:
                     query_args = None
-                if 'range' in task:
+                if 'range' in task:  # list类API
                     step = limit = func_limit  # use default values
                     if len(task['range']) == 2:
                         start, end = task['range']
@@ -109,34 +116,26 @@ async def _gather_result(work_flow: dict, func: callable, headers_pool: list,
                             offset = end - remainder
                             step = limit = remainder
                             ...
-                        import traceback
-                        try:
-                            url = func(
-                                identifier,
-                                offset,
-                                limit=limit,
-                                query_args=query_args)  # fixme:此处容易出错,但尚无提示
-                        except BaseException:
-                            logging.error(traceback.format_exc())
-
-                        task_pool.append(_fetch(url, identifier, func, session,
-                                                random.choice(headers_pool),
-                                                offset=offset, step=step,
-                                                limit=limit))
+                        url = func(identifier, offset, limit=limit,
+                                   query_args=query_args).url
+                        task_pool.append(
+                            _fetch(url, identifier, func, session,
+                                   random.choice(headers_pool),
+                                   offset=offset, step=step,
+                                   limit=limit))
                         ...
                     ...
-                else:  # 'range' is not in task:
-                    url = func(identifier, query_args=query_args)
+                else:  # info类API
+                    url = func(identifier, query_args=query_args).url
                     task_pool.append(_fetch(url, identifier, func, session,
                                             random.choice(headers_pool)))
                     ...
                 ...
-            result = await asyncio.gather(*task_pool, return_exceptions=True)
+            result = await asyncio.gather(*task_pool,
+                                          return_exceptions=True)
             return result
-    else:
-        logging.info(
-            f'Process\t{process_id}\ttask count {work_flow["task_count"]} is not positive.')
-        return 'NO TASK'
+    except BaseException:
+        logging.error(traceback.format_exc())
 
 
 def _sub_process(work_flow: dict, func: callable, headers_pool: list,
@@ -146,20 +145,18 @@ def _sub_process(work_flow: dict, func: callable, headers_pool: list,
     :param work_flow: work flow dict
     :param func: function that return url
     :param headers_pool: headers pool for random selection
-    :param json_list:
+    :param json_list: multiprocessing.Manager.list
     :param process_id:
     :return:
     """
     __new_loop = asyncio.new_event_loop()
-    # set 'new_loop' as event_loop for this thread/process
+    # set 'new_loop' as event_loop for current subprocess
     asyncio.set_event_loop(__new_loop)
     __data = __new_loop.run_until_complete(
         _gather_result(work_flow, func, headers_pool, process_id))
     __new_loop.close()
     if isinstance(__data, list):
         json_list.extend(__data)
-        ...
-    ...
 
 
 def get_data(fetch_body: list, func, process_num: int = 4,
@@ -185,9 +182,11 @@ def get_data(fetch_body: list, func, process_num: int = 4,
         flood_discharge_ratio=flood_discharge_ratio,
         floodplain_ratio=floodplain_ratio)  # 负载均衡和range参数校验
     for process_id in range(process_num):
-        pool.apply_async(_sub_process,
-                         [task_allocation[process_id], func, headers_pool,
-                          return_list, process_id])
+        if task_allocation[process_id]['task_count'] > 0:
+            pool.apply_async(_sub_process,
+                             [task_allocation[process_id], func, headers_pool,
+                              return_list, process_id])
+            ...
         ...
     pool.close()
     pool.join()
@@ -200,9 +199,8 @@ def get_data(fetch_body: list, func, process_num: int = 4,
             continue
         if 'paging' in each:
             identifier = each['paging']['identifier']
-        else:  # 加入对API中info的支持
+        else:  # info类API
             identifier = each['identifier']
-            ...
         if identifier in res_dict:
             # info类只返回一个dict,流向else
             # 若同时请求大量info,若请求时未做去重,重复结果将流入此
@@ -215,8 +213,6 @@ def get_data(fetch_body: list, func, process_num: int = 4,
                 pass
         else:
             res_dict[identifier] = each
-            ...
-        ...
     '''
     res_dict:
     {
@@ -224,8 +220,6 @@ def get_data(fetch_body: list, func, process_num: int = 4,
         {
             "paging":
             {
-                "is_end": "False",
-                "is_start": "True",
                 "totals": 2243453,
                 "identifier": "zhang-jia-wei"
             },
@@ -233,7 +227,6 @@ def get_data(fetch_body: list, func, process_num: int = 4,
             {
                 "url_token": "1999nian-de-xia-tian",
                 "id": "51651651",
-                "balabala":"balabala"
             }]
         },
         "imike": {balabala}
@@ -248,10 +241,9 @@ if __name__ == '__main__':
     ZHI = zhihu_APIs.ZhiHu()
     FUNC = ZHI.members.followers
 
-    FETCH_BODY = [
-        {"identifier": 'zhang-jia-wei', "query_args": ["following_count"],
-         "range": [0, 200]},
-        {"identifier": 'imike', "range": [0, 21, 20, 2]}, ]
+    FETCH_BODY = [{"identifier": 'imike', "query_args": ["following_count"],
+                   "range": [0, 10]},
+                  {"identifier": 'splitter', "range": [0, 10, 20, 2]}, ]
     RES = get_data(
         FETCH_BODY,
         FUNC,
